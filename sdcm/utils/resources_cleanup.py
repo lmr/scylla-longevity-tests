e24bce597e28a9b8391b859f0671164a7f5265c2
Israel Fruchter <fruch@scylladb.com>
2024-11-13 13:54:51 +0200

    feature(dedicated_hosts): cleanup dedicated hosts by tags
    doing so we can now clear the hosts with `hydra clean-resources`
    base on `user` or `test_id`

--- sdcm/utils/resources_cleanup.py
+++ sdcm/utils/resources_cleanup.py
@@ -90,7 +90,7 @@ def clean_cloud_resources(tags_dict, config=None, dry_run=False):
     if cluster_backend in ('aws', 'k8s-eks', ''):
         clean_instances_aws(tags_dict, regions=aws_regions, dry_run=dry_run)
         SCTCapacityReservation.cancel(config)
-        SCTDedicatedHosts.release(config)
+        SCTDedicatedHosts.release_by_tags(tags_dict, regions=aws_regions, dry_run=dry_run)
         clean_elastic_ips_aws(tags_dict, regions=aws_regions, dry_run=dry_run)
         clean_test_security_groups(tags_dict, regions=aws_regions, dry_run=dry_run)
         clean_placement_groups_aws(tags_dict, regions=aws_regions, dry_run=dry_run)
